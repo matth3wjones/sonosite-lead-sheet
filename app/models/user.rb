13b1d215f585b470b6fda6856a0f19fad8972323
Matthew Jones <matthewjones@Matthews-MacBook-Pro.local>
2017-05-25 11:27:23 -0700

    add authentication and user migration

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+  validates :username, :email, :password, presence: true
+  validates :email, uniqueness: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+
+  def authenticate(password)
+    return false if self == nil
+    self.password == password
+  end
+
+  def password
+    @password ||= BCrypt::Password.new(hashed_password)
+  end
+
+  def password=(password)
+    @password = BCrypt::Password.create(password)
+    self.hashed_password = @password
+  end
+end
